a312450b9a9f30b934df30e6d85abe5c352e4b42
rintaro.sawada <rinwashi321@gamil.com>
2022-03-02 08:20:20 +0000

    kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,30 @@
+def FizzBuzz(num)
+
+
+    if num%15==0
+    
+    return "FizzBuzz"
+    
+    elsif num%3==0
+      
+      return "Fizz"
+      
+    elsif num%5==0
+        
+        return "Buzz"
+        
+    else 
+        
+        return num
+        
+    end
+      
+end
+  
+num_max=100
+ 
+(1..num_max).each do |number|
+   
+   FizzBuzz(number)
+   
+end
